a053867b244dfcd61a1b4c5b264609b01378f1aa
likhita789 <likhita789@users.noreply.github.com>
2018-02-25 13:52:41 -0600

    Add files via upload

---
+++ BidService.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.wpl.bidding.service;
+
+/**
+ * @author Sushma
+ *
+ */
+public interface BidService {
+	
+	public void bidItem(int userId,float bidValue);
+
+}
---
+++ BiddingServiceImpl.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.wpl.bidding.service;
+
+/**
+ * @author Sushma
+ *
+ */
+public class BiddingServiceImpl implements BidService {
+
+	@Override
+	public void bidItem(int userId, float bidValue) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
